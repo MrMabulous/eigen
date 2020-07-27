974a9e06e3e6524cc08def9c35269a7d38098078
jwehner <jenswehner@gmail.com>
2020-07-27 15:38:54 +0200

    moved code to unsupported and fixed MR comments

--- unsupported/test/bicgstabl.cpp
+++ unsupported/test/bicgstabl.cpp
@@ -7,8 +7,8 @@
 // Public License v. 2.0. If a copy of the MPL was not distributed
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "sparse_solver.h"
-#include <Eigen/IterativeLinearSolvers>
+#include "../../test/sparse_solver.h"
+#include <unsupported/Eigen/IterativeSolvers>
 
 template<typename T, typename I_> void test_BiCGSTABL_T()
 {
